0272553bda9223e2134a60703af338cce10a9e85
Maksym Grebenets <maksym.grebenets@cba.com.au>
2018-03-02 14:11:33 +1100

    Update examples to work with CocoaPods

---
+++ Examples/Example.swift
@@ -0,0 +1,24 @@
+#!/usr/bin/env xcrun swift -F Carthage/Build/Mac
+
+// Shebangs for using with different dependency managers.
+//
+// - Carthage:
+// #!/usr/bin/env xcrun swift -F Carthage/Build/Mac
+// - CocoaPods Rome:
+// #!/usr/bin/env xcrun swift -F Rome
+// - Swift Packgage Manager:
+
+import Alamofire
+import SwiftScriptRunner
+
+var runner = SwiftScriptRunner()
+runner.lock() // Lock
+
+Alamofire.request("http://httpbin.org/get")
+    .responseJSON { response in
+         print("Successful response:")
+         print(response)   // Result of response serialization
+         runner.unlock() // Unlock
+    }
+
+runner.wait() // Wait
