41169267ee0750242d48373d2cf6225b21dcfbea
Maksym Grebenets <maksym.grebenets@cba.com.au>
2018-03-02 12:03:02 +1100

    Update for Xcode 9.2 and Swift 4

--- Pod/Classes/SwiftScriptRunner.swift
+++ Pod/Classes/SwiftScriptRunner.swift
@@ -4,27 +4,29 @@
 
 import Foundation
 
-/// Helper for running Swift scripts with async callbacks
+/// Helper for running Swift scripts with async callbacks.
 open class SwiftScriptRunner {
-    /// A poor man's mutex
-    fileprivate var count = 0
-    /// Current run loop
-    fileprivate let runLoop = RunLoop.current
 
-    /// Initializer
+    /// A poor man's mutex.
+    private var count = 0
+
+    /// Current run loop.
+    private let runLoop = RunLoop.current
+
+    /// Initializer.
     public init() {}
 
-    /// Lock the script
+    /// Lock the script runner.
     open func lock() {
         count += 1
     }
 
-    /// Unlock the script
+    /// Unlock the script runner.
     open func unlock() {
         count -= 1
     }
 
-    /// Wait for all locks to unlock
+    /// Wait for all locks to unlock.
     open func wait() {
         while count > 0 &&
             runLoop.run(mode: .defaultRunLoopMode, before: Date(timeIntervalSinceNow: 0.1)) {
